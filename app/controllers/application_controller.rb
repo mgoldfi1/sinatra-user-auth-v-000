ba96f5d03b4cfa51be331d9a988aa7b4722f7bb4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-08-06 17:58:27 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -30,6 +30,7 @@ class ApplicationController < Sinatra::Base
   post '/sessions' do
     @user = User.find_by(email: params["email"], password: params["password"])
     session[:id] = @user.id
+    if @user != nil
     redirect '/users/home'
   end
 
